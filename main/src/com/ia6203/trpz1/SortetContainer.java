3646c8dadbd7ff4a9f9544f454266de51a2c1235
HalitovskiySerhyi <32512235+halitovskiyserhyi@users.noreply.github.com>
2018-02-13 23:08:39 +0200

    add quick sort

--- main/src/com/ia6203/trpz1/SortetContainer.java
+++ main/src/com/ia6203/trpz1/SortetContainer.java
@@ -1,8 +1,62 @@
 package com.ia6203.trpz1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class SortetContainer {
 
+    static int[] quickArray;
+
+    public static void quickSort() {
+        quickArray=new int[200];
+        Random rand=new Random();
+        for(int i=0;i<quickArray.length;i++){
+            quickArray[i]=rand.nextInt(200);
+        }
+
+        Arrays.stream(quickArray).forEach((item)-> {
+            System.out.print(item+" ");
+        });
+        System.out.println();
+
+
+        int startIndex = 0;
+        int endIndex = 200 - 1;
+        doSort(startIndex, endIndex);
+
+        Arrays.stream(quickArray).forEach((item)-> {
+            System.out.print(item+" ");
+        });
+    }
+
+    private static void doSort(int start, int end) {
+        if (start >= end)
+            return;
+        int i = start, j = end;
+        int cur = i - (i - j) / 2;
+        while (i < j) {
+            while (i < cur && (quickArray[i] <= quickArray[cur])) {
+                i++;
+            }
+            while (j > cur && (quickArray[cur] <= quickArray[j])) {
+                j--;
+            }
+            if (i < j) {
+                int temp = quickArray[i];
+                quickArray[i] = quickArray[j];
+                quickArray[j] = temp;
+                if (i == cur)
+                    cur = j;
+                else if (j == cur)
+                    cur = i;
+            }
+        }
+        doSort(start, cur);
+        doSort(cur+1, end);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        quickSort();
+
     }
 }
