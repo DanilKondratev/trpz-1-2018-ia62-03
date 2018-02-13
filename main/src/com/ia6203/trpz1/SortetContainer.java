1daaae14a50eec0b5f68434fd9bbf20cfabfbf3c
Komarskiy <bionic99@list.ru>
2018-02-13 22:29:42 +0200

    My sort bubbleSort

--- main/src/com/ia6203/trpz1/SortetContainer.java
+++ main/src/com/ia6203/trpz1/SortetContainer.java
@@ -4,5 +4,20 @@ public class SortetContainer {
 
     public static void main(String[] args){
         System.out.println("Hello world!");
+
+    }
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
